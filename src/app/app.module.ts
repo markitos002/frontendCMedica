8eeaff228bebb9f5f32d298820720efdf5cd5554
Marco Fidel Avila <markos.avila@gmail.com>
2024-08-15 23:46:02 -0500

    update to angular core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'; // se imporata esta clase http para poder hacer peticiones http
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; // se imporata esta clase http para poder hacer peticiones http
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms'; // para que funcionen los formularios
@@ -27,35 +27,24 @@ import { PersonasService} from './pages/personas/service/personas.service';
 import { CitasService} from './pages/citas/service/citas.service'; // se importa el servicio
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    HomeComponent,
-    PersonasComponent,
-    CitasComponent,
-
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule, // se inicializa el modulo http para poder hacer peticiones http
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    MatRadioModule,
-
-
-  ],
-
-  providers: [
-    PersonasService,  // se importa el servicio en providers debido a que no se puede inyectar en el componente
-    CitasService],
-
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        NavComponent,
+        HomeComponent,
+        PersonasComponent,
+        CitasComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule, // se inicializa el modulo http para poder hacer peticiones http
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatSliderModule,
+        MatRadioModule], providers: [
+        PersonasService, // se importa el servicio en providers debido a que no se puede inyectar en el componente
+        CitasService,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
 export class AngularMaterialModule { }
 export class MaterialModule { }
