0eeb507c701160360449030c7e4ed35c87fec55a
Marco Fidel Avila <markos.avila@gmail.com>
2024-08-25 22:31:11 -0500

    update links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PersonasComponent } from './pages/personas/personas.component';
 import { CitasComponent } from './pages/citas/citas.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [  // se configuran las rutas principales
+const routes: Routes = [  // se configuran las rutas principales de la aplicación
+  {  path: '', redirectTo: '/home', pathMatch: 'full' },  // si la ruta es vacía se redirecciona a la ruta principal
   {  path: 'home', component: HomeComponent },
   {  path: 'citas', component: CitasComponent },
   {  path: 'personas', component: PersonasComponent },
-
-  {
-    path: '**', redirectTo: 'home'  // si no se encuentra la ruta se redirecciona a la ruta principal
-  }
-
+  {  path: 'login', component: LoginComponent},
+  {  path: '**', redirectTo: '/home' } // si no se encuentra la ruta se redirecciona a la ruta principal
 ];
 
 @NgModule({
